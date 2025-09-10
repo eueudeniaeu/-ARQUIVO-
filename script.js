c6fec8aebb251f3a0cd6e7afff59267958974ff7
Eudenia <eudenia.silva08@aluno.ifce.edu.br>
2025-09-10 17:46:45 -0300

    Formulários

---
+++ script.js
@@ -0,0 +1,9 @@
+function mostrarDados(){
+    var nome = document.getElementById("iptName").value;
+    var sb = document.getElementById("iptSobrenome").value;
+    var idade = document.getElementById("iptIdade").values;
+
+    idade = parseInt(idade);
+
+    var spnDados,innerText = nome + ' | ' + sb + ' | '
+}
