cf71efd7528c8518a2df5b31cb69382b609cbf5c
DataPlacebos <eudenia.silva08@aluno.ifce.edu.br>
2025-11-03 17:54:51 -0300

    Projeto 11

---
+++ Projeto AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA/js/script.js
@@ -0,0 +1,11 @@
+function somar() {
+    var iptX = document.querySelector("#iptX");
+    var x = iptX.ariaValueMax;
+
+    var iptY = document.querySelector("#iptY");
+    var y = iptY.value;
+
+    var spnRes = document.querySelector.Selector("#spnRes");
+
+    spnRes
+}
